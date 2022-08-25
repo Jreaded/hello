6c8dc3d05f8b10eb388b4dad80305d84a88660be
JR <jrhodes-winfrey2@huskers.unl.educdcdpwdgit config --global user.email jrhodes-winfrey2@huskers.unl.edu>
2022-08-25 19:29:28 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: JR
+ *         jrhodes-winfrey2@Huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("JRg");
+
+  printf("JR");
+  return 0;
+}
